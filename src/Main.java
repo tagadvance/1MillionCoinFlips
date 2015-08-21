64766c8e0f420983bc379182509579732b27d4fd
Tag <tagadvance@gmail.com>
2015-08-20 22:01:44 -0700

    comment

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * 
  * @author Tag <tagadvance@gmail.com>
- * @see https://www.reddit.com/r/askscience/comments/3hp4ig/
- *      if_i_flip_a_coin_1000000_times_what_are_the_odds/
+ * @see https://www.reddit.com/r/askscience/comments/3hp4ig/if_i_flip_a_coin_1000000_times_what_are_the_odds/
  */
 public class Main {
 
